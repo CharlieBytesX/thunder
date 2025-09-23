80718333f76f311855ffbba7545decc8fdbf8a75
🦈 Carlos Solórzano Cerdas <carlos.ace27@gmail.com>
2025-09-22 18:41:28 -0600

    wip: add validations

--- valipower-macros/src/lib.rs
+++ valipower-macros/src/lib.rs
@@ -4,7 +4,7 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{DeriveInput, parse_macro_input};
+use syn::{DeriveInput, Meta, Token, parse_macro_input, punctuated::Punctuated};
 
 #[proc_macro_derive(Validate, attributes(validate))]
 pub fn validate_derive(input: TokenStream) -> TokenStream {
@@ -13,16 +13,54 @@ pub fn validate_derive(input: TokenStream) -> TokenStream {
     let name = &input.ident;
     let fields = match &input.data {
         syn::Data::Struct(data_struct) => &data_struct.fields,
-        syn::Data::Enum(data_enum) => todo!(),
-        syn::Data::Union(data_union) => todo!(),
+        syn::Data::Enum(_) => todo!(),
+        syn::Data::Union(_) => todo!(),
     };
 
     let field_validators = fields.iter().map(|f| {
         let field_name = f.ident.as_ref().unwrap();
+        let mut field_checks = Vec::new();
+
+        for attribute in &f.attrs {
+            if !attribute.path().is_ident("valipower") {
+                continue;
+            }
+            // This parses the comma-separated list inside `validate(...)`
+            let nested = attribute
+                .parse_args_with(Punctuated::<Meta, Token![,]>::parse_terminated)
+                .unwrap();
+
+            for meta in nested {
+                match meta {
+                // Case 1: A simple path validator like `#[validate(email)]`
+                    Meta::Path(path) if path.is_ident("email") => {
+                        let check = quote! {
+                            // `self.#field_name` refers to the field's value
+                            if !self.#field_name.contains('@') {
+                                 errors.add(stringify!(#field_name), "email", "Must be a valid email address.");
+                            }
+                        };
+                        field_checks.push(check);
+                    }
+
+                    // Case 2: A list validator like `#[validate(length(min=3))]`
+                    Meta::List(list) if list.path.is_ident("length") => {
+                        // This is where you'd parse the arguments `min=3, max=20`
+                        // For brevity, we will implement this next
+                    }
+                    
+                    // You would add more `Meta` arms for `range`, `alphanumeric`, etc.
+
+                    _ => {} // Ignore unknown validation rules
+
+                }
+            }
+        }
+
         quote! {}
     });
 
-    let expaned = quote! {
+    let expanded = quote! {
         impl valipower::Validate for #name{
 
             fn validate(&self)-> Result<(),valipower::ValidationErrors>{
